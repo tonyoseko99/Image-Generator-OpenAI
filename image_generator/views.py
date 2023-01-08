044960c17b9de3e9fc76770cc0c2c89eaf058bee
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-09 00:00:48 +0300

    Add the API key and install openAI library

--- image_generator/views.py
+++ image_generator/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import requests
 
 # Create your views here.
+API_KEY = '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'
+
+def image(request):
+    response = requests.post(
+        'https://api.openai.com/v1/images/generations',
+        headers={ 'Authorization': 'Bearer ' + API_KEY },
+    )
+
