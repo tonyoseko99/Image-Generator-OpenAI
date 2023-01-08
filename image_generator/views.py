10672deb07f191fe7126f564df4adefa1854fe58
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-09 00:11:58 +0300

    Add the headers and json return output format

--- image_generator/views.py
+++ image_generator/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import requests
+import os
+import openai
+
 
 # Create your views here.
 API_KEY = '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'
 
+
 def image(request):
     response = requests.post(
         'https://api.openai.com/v1/images/generations',
-        headers={ 'Authorization': 'Bearer ' + API_KEY },
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + API_KEY
+        },
+        json={
+            "model": "text-davinci-003",
+            "prompt": "this is a test",
+            "temperature": 0,
+            "max_tokens": 7
+        }
     )
-
