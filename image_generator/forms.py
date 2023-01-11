7fefe525d41c50c012422d6452779f7de48090af
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-11 13:18:53 +0300

    render the images array in the form.html file

--- image_generator/forms.py
+++ image_generator/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class ImageForm(forms.Form):
     prompt = forms.CharField(label='Prompt', max_length=100,
                              widget=forms.TextInput(attrs={'class': 'text-field'}))
-    n = forms.IntegerField(label='Number of images', min_value=2, error_messages={'min_value': "Minimum 2 Images is required"}, widget=forms.NumberInput(
-        attrs={'class': 'integer-field'}))
+    n = forms.IntegerField(label='Number of images',
+                           widget=forms.NumberInput(attrs={'class': 'integer-field'}))
     size = forms.CharField(label='Size', max_length=100,
                            widget=forms.TextInput(attrs={'class': 'text-field'}))
--- image_generator/views.py
+++ image_generator/views.py
@@ -4,6 +4,7 @@ import requests
 
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib import messages
 from .forms import ImageForm
 
 # set the API key as an environment variable
@@ -12,26 +13,31 @@ os.environ["OPENAI_API_KEY"] = "sk-A4I5e0fBv7AhFtu8C8t3T3BlbkFJgMDbXYoqUBGEqG8oP
 # use the os.getenv() function to reference the API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 def handle_form_submission(request):
     if request.method == 'POST':
         form = ImageForm(request.POST)
         if form.is_valid():
             # get the data from the form
             prompt = form.cleaned_data['prompt']
-            n = form.cleaned_data['n']
+            n_images = form.cleaned_data['n']
             size = form.cleaned_data['size']
 
             # Use the data from the form to generate the image
             response = openai.Image.create(
                 prompt=prompt,
-                n=n,
+                n=n_images,
                 size=size
             )
 
             image_urls = [image['url'] for image in response['data']]
+            # print the image URLs to the console for debugging
+            print(image_urls)
+            # print the number of objects in the response to the console for debugging
+            print(len(response['data']))
 
             # render the image in the browser
-            return render(request, 'form.html', {'image_url': image_urls, "description": prompt})
+            return render(request, 'form.html', {'image_urls': image_urls})
     else:
         form = ImageForm()
 
